da8368298501d52fba9b69d58a69d52d262e1adb
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-03-22 23:30:40 -0300

    fixed a bug where draw didnt end the game

--- rpsgame.js
+++ rpsgame.js
@@ -1,76 +1,80 @@
+// Base Scores to add
 let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
-const maxPoints = 5;
-document.getElementById("playagain").style.display = "none";
+const maxPoints = 5; // Max Points
+document.getElementById("playagain").style.display = "none"; // Play again button hidden
 
+// Simple function to generate a random number to decide computer choice
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        console.log('Computer chose Rock!')
+        document.getElementById('consoleComputerChoice').textContent = 'Computer chose Rock!';
         return "rock"
     } else if (randomNumber === 1) {
-        console.log('Computer chose Paper!')
+        document.getElementById('consoleComputerChoice').textContent = 'Computer chose Paper!';
         return "paper"
     } else {
-        console.log('Computer chose Scissors!')
+        document.getElementById('consoleComputerChoice').textContent = 'Computer chose Scissors!';
         return "scissors"
     };
 };
 
-function getHumanChoice(choice) {
-    if (choice === 'rock') {
-        console.log('You choose Rock.')
-        return "rock"
-    }   else if (choice === 'paper') {
-        console.log('You choose Paper.')
-        return "paper"
-    }   else if (choice === 'scissors') {
-        console.log('You choose Scissors.')
-        return "scissors"
-    }   else {
-        console.log("That's not an option.")
-    };
-};
-
-function playRound(humanChoice, computerChoice) {
-    
+// Function which gets the results from clicking on the buttons to play a round.
+function playRound(humanChoice, computerChoice) {    
     computerChoice = getComputerChoice();
     if (humanChoice === "rock" && computerChoice === "scissors") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Rock.';
         document.getElementById('consoleText').textContent = "You scored a point!";
         humanScore++;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Paper.';
         document.getElementById('consoleText').textContent = "You scored a point!";
         humanScore++;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Scissors.';
         document.getElementById('consoleText').textContent = "You scored a point!";
         humanScore++;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Paper.';
         document.getElementById('consoleText').textContent = "The computer scored a point!";
         computerScore++;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Scissors.';
         document.getElementById('consoleText').textContent = "The computer scored a point!";
         computerScore++;
     } else if (humanChoice === "rock" && computerChoice === "paper") {
+        document.getElementById('consoleHumanChoice').textContent = 'You choose Rock.';
         document.getElementById('consoleText').textContent = "The computer scored a point!";
         computerScore++;
-    } else if (humanChoice === computerChoice) {
-        document.getElementById('consoleText').textContent = "It's a draw!";
-        drawScore++;
-        document.getElementById('drawScore').textContent = "DRAWS: "+ drawScore
-        return "draw"
     };
-    document.getElementById('playerScore').textContent = "PLAYER: "+ humanScore
-    document.getElementById('computerScore').textContent = "COMPUTER: "+ computerScore
+    // Draw possibilities
+    if (humanChoice === computerChoice) {
+        document.getElementById('consoleHumanChoice').textContent = '';
+        document.getElementById('consoleComputerChoice').textContent = '';
+        document.getElementById('consoleText').textContent = "Both players selected the same! It's a draw!";
+        drawScore++;
+        document.getElementById('drawScore').textContent = "DRAWS: "+ drawScore;
+        if (drawScore === maxPoints) {
+            document.getElementById('consoleText').textContent = "The Game is over, click on the button to play again!";
+            document.getElementById("rockbutton").style.display = "none";
+            document.getElementById("scissorsbutton").style.display = "none";
+            document.getElementById("paperbutton").style.display = "none";
+            document.getElementById("playagain").style.display = "block"; 
+    }; // Max points comparator has to be inside draw possibilites to work properly
+        return "draw";
+    };
 
-    if (humanScore === maxPoints || computerScore === maxPoints || drawScore === maxPoints) {
+    document.getElementById('playerScore').textContent = "PLAYER: "+ humanScore;
+    document.getElementById('computerScore').textContent = "COMPUTER: "+ computerScore;
+
+    if (humanScore === maxPoints || computerScore === maxPoints) {
         document.getElementById('consoleText').textContent = "The Game is over, click on the button to play again!";
         document.getElementById("rockbutton").style.display = "none";
         document.getElementById("scissorsbutton").style.display = "none";
         document.getElementById("paperbutton").style.display = "none";
-        document.getElementById("playagain").style.display = "block";
-        
-    }
+        document.getElementById("playagain").style.display = "block"; 
+    };
 };
 
 function resetGame(buttonClick) {
@@ -78,14 +82,16 @@ function resetGame(buttonClick) {
         humanScore = 0;
         computerScore = 0;
         drawScore = 0;
-        document.getElementById('playerScore').textContent = "PLAYER: "+ humanScore
-        document.getElementById('computerScore').textContent = "COMPUTER: "+ computerScore
-        document.getElementById('drawScore').textContent = "DRAWS: "+ drawScore
-        
+        document.getElementById('consoleHumanChoice').textContent = '';
+        document.getElementById('consoleComputerChoice').textContent = '';
+        document.getElementById('consoleText').textContent = "FIRST TO FIVE POINTS WIN! GOOD LUCK!";
+        document.getElementById('playerScore').textContent = "PLAYER: "+ humanScore;
+        document.getElementById('computerScore').textContent = "COMPUTER: "+ computerScore;
+        document.getElementById('drawScore').textContent = "DRAWS: "+ drawScore;
         document.getElementById("rockbutton").style.display = "block";
         document.getElementById("scissorsbutton").style.display = "block";
         document.getElementById("paperbutton").style.display = "block";
         document.getElementById("playagain").style.display = "none";
-    }
+    };
 
-}
+};
